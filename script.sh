b023153eda24031b8910dadf1db4d98096f6172b
tenglor <isdigni@gmail.com>
2026-02-03 23:14:03 +0300

    add pass existing files

--- script.sh
+++ script.sh
@@ -71,9 +71,15 @@ for font in "${fonts[@]}"; do
     fi
 done
 
+mkdir -p "${target_dir}/converted"
+
 for input in "${target_dir}"/*.mkv; do
-	echo "Video $input"
+	echo "Video $input"	
 	input_base="$(basename "$input" .mkv)"
+	if [ -e "${target_dir}/converted/${input_base}.mkv" ]; then
+		echo "Output video ${target_dir}/converted/${input_base}.mkv exist. Passed"	
+		continue
+	fi		
 	echo "$input_base"
 	input_sub=()
 	input_audio=()
@@ -91,25 +97,29 @@ for input in "${target_dir}"/*.mkv; do
             echo "Найдено аудио: $(basename "$audio")"
         fi
     done
-	output_mkv="${target_dir}/${input_base}-out.mkv"
+	output_mkv="${target_dir}/converted/${input_base}.mkv"
 	output_mkv_tmp="${target_dir}/tmp.mkv"
 	echo "input_mkv: $input"	
 	echo "sub 0: ${input_sub[0]}"	
-	echo "sub 1: ${input_sub[1]}"	
-	echo "audio 0: ${input_audio[0]}"	
-	echo "audio 1: ${input_audio[1]}"	
+	echo "sub 1: ${input_sub[1]}"		
 	echo "output_mkv: $output_mkv"
 	if [[ ${#fonts[@]} -eq 0 ]]; then
-		ffmpeg -i "$input" -i "${input_sub[0]}" -i "${input_sub[1]}" -i "${input_audio[0]}" -i "${input_audio[1]}" -y \
+		ffmpeg -v quiet  -stats -i "$input" -i "${input_sub[0]}" -i "${input_sub[1]}"  -y \
 			-c:v hevc_nvenc -rc vbr -cq 22 -profile:v main -pix_fmt yuv420p  -preset slow -c:s copy \
 			-c:a aac -b:a 192k -ac 2  \
-			-map 0:v:0 -map 0:a -map 3:a:0 -map 4:a:0 -map 2:s:0 -map 1:s:0 -ignore_unknown \
+			-map 0:v:0 -map 0:a  -map 2:s:0 -map 1:s:0 -ignore_unknown \
 			"$output_mkv"
 	else
-		ffmpeg -i "$input" -i "${input_sub[0]}" -i "${input_sub[1]}" -i "${input_audio[0]}" -i "${input_audio[1]}" -y \
+		ffmpeg -i "$input" \
+			-i "${input_sub[0]}" -i "${input_sub[1]}" -i "${input_sub[2]}"  -i "${input_sub[3]}" \
+			-i "${input_audio[0]}" -i "${input_audio[1]}" -i "${input_audio[2]}"  -i "${input_audio[3]}" \
+			-y \
 			-c:v hevc_nvenc -rc vbr -cq 22 -profile:v main -pix_fmt yuv420p  -preset slow -c:s copy \
 			-c:a aac -b:a 192k -ac 2  \
-			-map 0:v:0 -map 0:a -map 3:a:0 -map 4:a:0 -map 2:s:0 -map 1:s:0 -ignore_unknown \
+			-map 0:v:0 -map 0:a \
+			-map 1:s:0 -map 2:s:0 -map 3:s:0 -map 4:s:0 \
+			-map 5:a:0 -map 6:a:0 -map 7:a:0 -map 8:a:0 \
+			-ignore_unknown \
 			"$output_mkv_tmp"
 		mkvmerge -o "$output_mkv" \
     		"$output_mkv_tmp" \
